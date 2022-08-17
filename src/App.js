8e64755e596562f6c70ac725f3b7f60c2c1b393e
higorbuueno <buenoshigor@gmail.com>
2022-08-17 19:50:10 -0300

    Primeira edição

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Higor é o brabo</p>
         <a
           className="App-link"
           href="https://reactjs.org"
